7a2d35c1c30af78c3d9083718971156b9532dc94
adwait <godbole15@gmail.com>
2021-07-30 19:35:51 -0700

    sum invariant

--- examples/my-examples/tracegen.py
+++ examples/my-examples/tracegen.py
@@ -1,12 +1,16 @@
 import sys
 
-# w_seq = [4, 3, 2, 1, 2, 4, 1, 4, 1, 98, 12, 1]
-# x_seq = [1, 4, 3, 2, 4, 4, 1, 2, 1, 23, 1, 1]
-# y_seq = [4, 1, 3, 1, 3, 1, 2, 1, 1, 8, 9, 12]
+w_seq = [4, 3, 2, 1, 2, 4, -1, 4, 1, 98, 12, 1, 4, 5, 4, 54]
+x_seq = [1, 4, 3, 2, 4, 4, 1, 2, 1, -23, 1, 1, 8, 3, 1, 4]
+y_seq = [4, 1, 3, 1, -3, 1, 2, 1, 1, 8, 9, 12, 6, 2, 4, 6]
 
-w_seq = [4, 2, 2, 2, 2, 4, 2, 4, 42, 98, 12, 8]
-x_seq = [12, 4, 34, 2, 4, 4, 6, 2, 12, 22, 12, 10]
-y_seq = [4, 1, 3, 1, 3, 1, 2, 1, 1, 8, 9, 12]
+w_seq = [4, 2, 2, 2, 2, 4, 2, 4, 42, 98, 12, 8, 2, 10, 52, 2, 4, 2]
+x_seq = [12, 4, 34, 2, 4, 4, 6, 2, 12, 22, 12, 10, 6, 2,2, 2, 32, 2]
+y_seq = [4, 1, 3, 1, 3, 2, 2, 2, 2, 8, 94, 12, 52, 6, 4, 6, 8, 4]
+
+# w_seq = [8, 4, 1, 21]
+# x_seq = [3, 4, 4, 20]
+# y_seq = [3, 6, 2, 21]
 
 
 def create_enter_block (wv, xv, yv, zv) -> str:
@@ -52,13 +56,17 @@ decl-version 2.0
 var-comparability implicit
 '''
 
-filename = 'sample1.dtrace'
+def main(args):
+    filename = args[0]
+    print(filename)
+    with open(filename, 'w') as fileh:
+        fileh.write(create_preamble())
 
-with open(filename, 'w') as fileh:
-    fileh.write(create_preamble())
+        for i in range(len(w_seq)):
+            wv, xv, yv = w_seq[i], x_seq[i], y_seq[i]
+            fileh.write(create_enter_block(wv, xv, yv, 'nonsensical'))
+            fileh.write(create_exit_block(wv, xv, yv, wv+xv+yv))
 
-    for i in range(len(w_seq)):
-        wv, xv, yv = w_seq[i], x_seq[i], y_seq[i]
-        fileh.write(create_enter_block(wv, xv, yv, 'nonsensical'))
-        fileh.write(create_exit_block(wv, xv, yv, wv+xv+yv))
 
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ EXAMPLE_DIR=$DAIKONDIR/examples/my-examples
 
 INV_CONFIG=(
     --user-defined-invariant daikon.inv.unary.scalar.Even
+    --user-defined-invariant daikon.inv.ternary.threeScalar.Sum
 )
 
+echo python3 $EXAMPLE_DIR/tracegen.py $EXAMPLE_DIR/sample1.dtrace
+python3 $EXAMPLE_DIR/tracegen.py $EXAMPLE_DIR/sample1.dtrace
+
 java -cp "$DAIKONDIR/daikon.jar:$DAIKONDIR/synthinv" daikon.Daikon "${INV_CONFIG[@]}" --config run.config $EXAMPLE_DIR/sample1.dtrace $EXAMPLE_DIR/sample.decls
+# java -cp "$DAIKONDIR/daikon.jar" daikon.Daikon --config run.config $EXAMPLE_DIR/sample1.dtrace $EXAMPLE_DIR/sample.decls
